bd80dc7d36138c72296f21b4c85726e2de01306a
Francisco Cortes <nero@Franciscos-MacBook-Air.local>
2026-09-07 11:16:53 -0300

    fix: corrige prueba del frontend

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -16,6 +16,6 @@ describe('App', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent)
-      .toContain('Título incorrecto');
+      .toContain('Catálogo de Recursos');
   });
 });
